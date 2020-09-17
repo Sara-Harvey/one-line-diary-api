d279d41650e27a421289787d8e562da80e1b7853
Sara Harvey <saraharveynyc@gmail.com>
2020-09-17 18:04:12 -0400

    commented out unneeded code in controllers, saving for later

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,5 +1,4 @@
 class EntriesController < ApplicationController
-#before_action :find_entry, only: [:show, :edit, :update, :destroy]
 
 	def index
 	  entries = Entry.all
@@ -9,15 +8,16 @@ class EntriesController < ApplicationController
 	  render json: EntrySerializer.new(entries)
 	end
 
-	def show
-	  entry = Entry.find(params[:id])
-	  render json: entry, status: 200 
-	end
+
+	#def show
+	#  entry = Entry.find(params[:id])
+	#  render json: entry, status: 200 
+	#end
 
 	def create
-	  entry = Entry.create(entry_params)
-	  if Entry.save
-	  	render json: entry, status: :accepted
+	  entry = Entry.new(entry_params)
+	  if entry.save
+	  	render json: EntrySerializer.new(entry), status: :accepted
 	  else
 	  	render json: {errors: entry.errors.full_messages}, status:
 	  	:unprocessible_entity
@@ -25,32 +25,30 @@ class EntriesController < ApplicationController
 	end
 
 # necessary @
-	def update
-        @entry.update(entry_params)
-       if @entry.save
-    	render json: @entry, status: 200
-       else
-    	render json: { errors: @entry.errors.full_messages }, status: :unprocessible_entity
-       end
-  	end
+	#def update
+    #    @entry.update(entry_params)
+    #   if @entry.save
+    #	render json: @entry, status: 200
+    #   else
+    #	render json: { errors: @entry.errors.full_messages }, status: :unprocessible_entity
+    #   end
+  	#end
 
-	def destroy
-		entry = Entry.find_by(id: params[:id])
-		entry.destroy
-		render json: entry
-	end
-
+	#def destroy
+	#	entry = Entry.find_by(id: params[:id])
+	#	entry.destroy
+	#	render json: entry
+	#end
     
     private
-
 
       def entry_params
         params.require(:entry).permit(:date, :content, :type_id)
       end
 
 # necessary @
-      def find_entry
-    	@entry = Entry.find(params[:id])
-      end
+    #  def find_entry
+    #	@entry = Entry.find(params[:id])
+    #  end
 
 end
--- app/controllers/types_controller.rb
+++ app/controllers/types_controller.rb
@@ -1,5 +1,4 @@
 class TypesController < ApplicationController
-	#before_action :find_type, only: [:show, :edit, :update, :destroy]
 	
 	def index
         types = Type.all
@@ -7,45 +6,44 @@ class TypesController < ApplicationController
     end
 
 
-	def show
-        @type = Type.find(params[:id])
-        render json: @type, status: 200
-    end 
+	#def show
+    #    @type = Type.find(params[:id])
+    #    render json: @type, status: 200
+    #end 
 
     def create
-      entry = Type.create(type_params)
-      if Type.save
-        render json: entry, status: :accepted
+      type = Type.create(type_params)
+      if type.save
+        render json: TypeSerializer.new(type), status: :accepted
       else
-        render json: {errors: entry.errors.full_messages}, status:
+        render json: {errors: type.errors.full_messages}, status:
         :unprocessible_entity
       end
     end
 
 
-    def update 
-        @type.update(type_params)
-        if @type.save
-          render json: @type, status: 200
-        else
-          render json: { errors: @type.errors.full_messages }, status: :unprocessible_entity
-        end
-    end 
+    #def update 
+    #    @type.update(type_params)
+    #    if @type.save
+    #      render json: @type, status: 200
+    #    else
+    #      render json: { errors: @type.errors.full_messages }, status: :unprocessible_entity
+    #    end
+    #end 
 
-    def destroy
-        type = Type.find_by(id: params[:id])
-        type.destroy
-        render json: type
-    end
-
+    #def destroy
+    #    type = Type.find_by(id: params[:id])
+    #    type.destroy
+    #    render json: type
+    #end
 
     private
 
     def type_params
-        params.permit(:title, :description, :entry_id)
+        params.require(:type).permit(:name)
     end
 
-    def find_type
-        @type = Type.find(params[:id])
-    end
+    #def find_type
+    #    @type = Type.find(params[:id])
+    #end
 end
