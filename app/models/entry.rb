33afc5ba521602569c9430f5dd6c05197a6208f3
Sara Harvey <saraharveynyc@gmail.com>
2020-10-24 09:58:43 -0400

    Update entry.rb
    removed old notes

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -2,6 +2,3 @@ class Entry < ApplicationRecord
   belongs_to :category
   accepts_nested_attributes_for :category
 end
-
-# collection_select(object, method, collection, value_method, 
-# text_method, options = {}, html_options = {})
